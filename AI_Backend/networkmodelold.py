39588eaab63e3b0a343d553e3760864a6413fcbf
tudortrita <tmt116@ic.ac.uk>
2019-01-26 20:40:59 +0000

    Added backend

--- AI_Backend/networkmodelold.py
+++ AI_Backend/networkmodelold.py
@@ -7,44 +7,40 @@ import matplotlib.pyplot as plt
 import random
 import scipy.misc
 import sys
-import tensorflow as tf
+
 # Best run: 0.9901
 
-# To neatly handle global variables later
-sess = tf.InteractiveSession()
-
 #Parameters:
-pixels1 = 100
-pixels2 = 100
+pixels = 100
 
 #No of training images:
-train_im_no = 982
-train_type1 = 492   # Training images of type 1
-train_type1_loc = "trainingdata/Apple Red 1/"
-train_type2 = 490   # Training images of type 2
-train_type2_loc = "trainingdata/Banana/"
+train_im_no = 932
+train_type1 = 491   # Training images of type 1
+train_type1_loc = "training/appletrain"
+train_type2 = 441   # Training images of type 2
+train_type2_loc = "training/bananatrain"
 
 #No of test images
-test_im_no = 330
-test_type1 = 164     # Test images of type 1
-test_type1_loc = "testdata/Apple Red 1/"
-test_type2 = 166     # Test images of type 2
-test_type2_loc = "testdata/Banana/"
+test_im_no = 91
+test_type1 = 42     # Test images of type 1
+test_type1_loc = "test/appletest"
+test_type2 = 49     # Test images of type 2
+test_type2_loc = "test/bananatest"
 
 #No of colours (B/W: 1, RGB: 3)
 colours = 3
 
 #Loading images:
-imtrain = np.zeros((train_im_no,pixels1,pixels2,colours))
+imtrain = np.zeros((train_im_no,pixels,pixels,colours))
 
 for i in range(train_type1):
     imtrain[i] = scipy.misc.imread(train_type1_loc + str(i+1) + ".jpg",mode="RGB")
 for i in range(train_type2):
     imtrain[i+train_type1] = scipy.misc.imread(train_type2_loc + str(i+1) + ".jpg",mode="RGB")
-
+    
 #imtrain is a list with train_im_no images inside at the moment
 #imtrain = [as.list(image) for image in imtrain]
-imtest = np.zeros((test_im_no,pixels1,pixels2,colours))
+imtest = np.zeros((test_im_no,pixels,pixels,colours))
 
 for i in range(test_type1):
     imtest[i] = scipy.misc.imread(test_type1_loc + str(i+1) + ".jpg",mode="RGB")
@@ -63,8 +59,8 @@ y_test[test_type2:] = 1
 (x_train, y_train), (x_test, y_test) = (imtrain, y_train), (imtest, y_test)
 
 #Format images
-x_train = x_train.reshape(train_im_no, pixels1, pixels2, colours)
-x_test = x_test.reshape(test_im_no, pixels1, pixels2, colours)
+x_train = x_train.reshape(train_im_no, pixels, pixels, colours)
+x_test = x_test.reshape(test_im_no, pixels, pixels, colours)
 x_train = x_train.astype('float32') #use floats so we can scale below
 x_test = x_test.astype('float32')
 
@@ -77,7 +73,7 @@ y_train = np_utils.to_categorical(y_train, 2)
 y_test = np_utils.to_categorical(y_test, 2)
 
 # Initialise layers
-inputlayer = Input(shape = (pixels1, pixels2, colours))
+inputlayer = Input(shape = (pixels, pixels, colours))
 #hidden = Conv2D(32, 8, strides=(1, 1), activation='tanh')(inputlayer)
 #hidden = MaxPooling2D()(hidden)
 #hidden = Conv2D(32, 8, strides=(1, 1), activation='tanh')(hidden)
@@ -92,29 +88,6 @@ model = Model(inputlayer, outputlayer)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(x_train, y_train, batch_size=32, epochs=5)
 
-tensor_info_x = tf.saved_model.utils.build_tensor_info(inputlayer)
-tensor_info_y = tf.saved_model.utils.build_tensor_info(outputlayer)
-
-prediction_signature = (
-        tf.saved_model.signature_def_utils.build_signature_def(
-                inputs={'images': tensor_info_x},
-                outputs={'scores': tensor_info_y},
-                method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME))
-
-#SAVE MODEL
-export_path = 'C:/Users/Tudor Trita/Desktop/ichack2019/AI_Backend/SavedModelFolder'
-print('Exporting trained model to', export_path)
-builder = tf.saved_model.builder.SavedModelBuilder(export_path)
-builder.add_meta_graph_and_variables(
-      sess, [tf.saved_model.tag_constants.SERVING], signature_def_map={'predict_images':prediction_signature})
-builder.save()
-
-
-
-
-
-
-
 # Evaluate trained network
 score = model.evaluate(x_test, y_test, batch_size=32)
 print('Test score:', score[0])
@@ -127,16 +100,7 @@ y_test = np.argmax(y_test, axis=1) #undo one-hot encoding on y_test, so we can c
 correct_indices = np.nonzero(predicted_classes == y_test)[0]
 incorrect_indices = np.nonzero(predicted_classes != y_test)[0]
 
-
-sess.close()
 sys.exit()
-
-
-
-
-
-
-
 
 # See some incorrectly-classes images
 fig, axes = plt.subplots(nrows=3, ncols=3)
@@ -144,12 +108,6 @@ fig.tight_layout()
 random.shuffle(incorrect_indices)
 for i, incorrect in enumerate(incorrect_indices[:9]):
     plt.subplot(3,3,i+1)
-    plt.imshow(x_test[incorrect].reshape(pixels1,pixels2), cmap='gray', interpolation='none')
+    plt.imshow(x_test[incorrect].reshape(pixels,pixels), cmap='gray', interpolation='none')
     plt.title('Predicted %s, Actual %s' %(predicted_classes[incorrect], y_test[incorrect]))
 plt.show()
-
-
-
-
-
-
