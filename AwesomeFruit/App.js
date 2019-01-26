7074e7b1d7baf139f913c9f5f2544d5bf573e10f
Gavin S <gavin@gavinseegoolam.co.uk>
2019-01-26 13:08:53 +0000

    added nav

--- AwesomeFruit/App.js
+++ AwesomeFruit/App.js
@@ -1,21 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator,
+AsyncStorage,
+} from 'react-native';
+
+import {Router, Scene} from 'react-native-router-flux';
+
+
+import Scanner from './src/screens/Scanner.js';
+import Home from './src/screens/Home.js';
+import ScannerModal from './src/screens/ScannerModal.js';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+    constructor() {
+        super();
+        this.state = { hasToken: false, isLoaded: false };
+    }
+    componentDidMount() {
+        AsyncStorage.getItem('token').then((token) => {
+          this.setState({ hasToken: token !== null, isLoaded: true })
+        });
+    }
+    render() {
+        if (!this.state.isLoaded) {
+          return (
+            <ActivityIndicator />
+          )
+        } else {
+            return (
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+              <Router>
+                    <Scene key='root'>
+                        <Scene
+                        component={Home}
+                        hideNavBar={true}
+                        initial={true}
+                        key='Home'
+                        title='Home'
+                        type='reset'
+                        initial={!this.state.hasToken}
+                        />
+                        <Scene
+                        component={Scanner}
+                        hideNavBar={true}
+                        key='Scanner'
+                        title='Scanner'
+                        type='reset'
+                        initial={this.state.hasToken}
+                        />
+
+
+                        <Scene key="ScannerModal" component={ScannerModal} hideNavBar={false} />
+                    </Scene>
+              </Router>
+            );
+        }
+    }
+
+}
